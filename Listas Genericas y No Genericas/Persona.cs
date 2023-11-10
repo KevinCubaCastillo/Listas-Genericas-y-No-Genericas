a6bcd27ef18dd36d90f65295217870e3471913ae
Kevin Cuba Castillo <cck2023657@est.univalle.edu>
2023-11-09 21:30:57 -0400

    Agregar archivos de proyecto.

---
+++ Listas Genericas y No Genericas/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listas_Genericas_y_No_Genericas
+{
+    public class Persona
+    {
+        public string ci { get; set; }
+        public string name { get; set; }
+        public Persona()
+        {
+            ci = "No Definido";
+            name = "No Definido";
+        }
+        public Persona(string ci, string name)
+        {
+            this.ci = ci;
+            this.name = name;
+        }
+    }
+}
---
+++ Listas Genericas y No Genericas/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Listas_Genericas_y_No_Genericas;
+using System.Collections;
+
+Persona oPersona1 = new Persona("6869086", "Kevin Cuba");
+
+
+int dato1 = 100;
+int dato2 = 200;
+int dato3 = 300;
+
+
+ArrayList listaNG = new ArrayList();
+listaNG.Add(dato1);
+listaNG.Add(dato2);
+listaNG.Add(dato3);
+
+
+List<int> listaPersonas = new List<int>();
+listaPersonas.Add(dato1);
+listaPersonas.Add(dato2);
+listaPersonas.Add(dato3);
